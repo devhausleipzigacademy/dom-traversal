acb81ce83d2843e13e0fc8ea4c6467acc2858cef
franzwollang <franzwollang@gmail.com>
2022-02-16 23:51:02 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+// Downwards
+//
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+// document.getElementsByName
+// document.querySelector
+// document.querySelectorAll
+// element.children
+
+// Upwards
+//
+// element.parentElement
+// element.closest
+
+// Sideways
+//
+// element.nextElementSibling
+// element.previousElementSibling
+// combine using parentElement, children, and index
+
+
+
+
+function changeColor(element) {
+    element.style.backgroundColor = 'yellow'
+}
+
+
+
+/*
+Exercise
+
+Practice traversing the DOM with the methods taught in this lesson. Uncomment the additional HTML in the index.html file and then do these tasks:
+
+    Select .characters with document.querySelector
+    Select .humans from .characters
+    Select all humans with querySelectorAll, starting from .humans
+    Select all hobbits with children
+    Select the Merry (the hobbit)
+    Select .enemies from Sauron
+    Select the .characters div from Nazgûl
+    Select Elrond from Glorfindel
+    Select Legolas from Glorfindel
+    Select Arwen from Glorfindel
+*/
+
+
+/*
+Exercise
+
+Can you replicate the behavior of all of these CSS selectors using JavaScript?
+
+https://www.w3schools.com/cssref/css_selectors.asp
+
+Root
+Universal
+Element
+Class
+ID
+Descendant
+Child
+Element + Class
+Multiple Elements
+Adjacent Sibling
+General Sibling
+First Child
+Last Child
+First of Type
+Only of Type
+Not
+
+Attribute Selectors?
+Pseudo Elements?
+
+See https://developer.mozilla.org/en-US/docs/Web/API/Element#methods
+
+*/
