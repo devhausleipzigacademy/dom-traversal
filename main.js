50bec133eeb99b9e25a1cb62d812be69cce3e79c
franzwollang <franzwollang@gmail.com>
2022-02-17 14:22:57 +0100

    Minor update

--- main.js
+++ main.js
@@ -20,13 +20,6 @@
 
 
 
-
-
-
-
-
-
-
 function changeColor(elements) {
     for (const element of elements){
         element.style.backgroundColor = 'yellow'
@@ -43,7 +36,7 @@ Practice traversing the DOM with the methods taught in this lesson. Uncomment th
     Select .humans from .characters
     Select all humans with querySelectorAll, starting from .humans
     Select all hobbits with children
-    Select the Merry (the hobbit)
+    Select Merry (the hobbit)
     Select .enemies from Sauron
     Select the .characters div from Nazgûl
     Select Elrond from Glorfindel
